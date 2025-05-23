bb0e003c430a79254a1f9dafc26c82a56c7c3298
Valentin Le Palud <valentin.lepalud@gmail.com>
2025-05-23 20:25:13 +0200

    day2

---
+++ day2/day2.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readFile() [][]int {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	var fileLines [][]int
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		var arr []int
+		for _, e := range strings.Split(line, " ") {
+			value, err := strconv.Atoi(e)
+			if err != nil {
+				log.Fatal(err)
+			}
+			arr = append(arr, value)
+		}
+		fileLines = append(fileLines, arr)
+	}
+	file.Close()
+
+	return fileLines
+}
+func main() {
+	values := readFile()
+	safeReports := 0
+	// part one
+	for _, report := range values {
+		if report[0] != report[1] {
+			isIncreasing := report[0] < report[1]
+			isSafe := true
+			for index, level := range report {
+				if index != 0 {
+					previousLevel := report[index-1]
+					switch {
+					case isIncreasing && level > previousLevel && level-previousLevel < 4:
+						{
+							// ok
+						}
+					case !isIncreasing && previousLevel > level && previousLevel-level < 4:
+						{
+							// ok
+						}
+					default:
+						isSafe = false
+					}
+				}
+			}
+			if isSafe {
+				safeReports++
+			}
+		}
+	}
+	fmt.Println("PART ONE =>", safeReports)
+
+	//part two
+	reallySafe := 0
+	for _, reportTmp := range values {
+		reportLength := len(reportTmp) + 1
+		isFullySafe := false
+		i := 0
+		for i < reportLength {
+			var report []int
+			for ind, el := range reportTmp {
+				if ind != i {
+					report = append(report, el)
+				}
+			}
+
+			i++
+			if report[0] != report[1] {
+				isIncreasing := report[0] < report[1]
+				isSafe := true
+				for index, level := range report {
+					if index != 0 {
+						previousLevel := report[index-1]
+						switch {
+						case isIncreasing && level > previousLevel && level-previousLevel < 4:
+							{
+								// ok
+							}
+						case !isIncreasing && previousLevel > level && previousLevel-level < 4:
+							{
+								// ok
+							}
+						default:
+							isSafe = false
+						}
+					}
+				}
+				if isSafe {
+					isFullySafe = true
+				}
+			}
+		}
+		if isFullySafe {
+			reallySafe++
+		}
+	}
+	fmt.Println("PART TWO =>", reallySafe)
+}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module advent_of_code_2024
-
-go 1.23.3
